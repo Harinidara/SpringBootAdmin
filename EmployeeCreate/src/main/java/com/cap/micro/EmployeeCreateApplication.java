a7990c912af67d955ce0705de016bcd3df2d75bc
harin <harin@DESKTOP-EBASGFE>
2020-04-17 19:55:21 +0530

    commit

---
+++ EmployeeCreate/src/main/java/com/cap/micro/EmployeeCreateApplication.java
@@ -0,0 +1,17 @@
+package com.cap.micro;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+
+@SpringBootApplication
+public class EmployeeCreateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeCreateApplication.class, args);
+	}
+
+}
